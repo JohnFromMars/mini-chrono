65dedd5be9f633b8ed1c611b4fe650ca7e4a3591
JohnFromMars <rock13810@gmail.com>
2024-06-24 10:25:46 +0800

    init

---
+++ src/main/java/com/johnfrommars/minichrono/MiniChronoApplication.java
@@ -0,0 +1,13 @@
+package com.johnfrommars.minichrono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniChronoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniChronoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnfrommars/minichrono/MiniChronoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnfrommars.minichrono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniChronoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
